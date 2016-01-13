e8f20fa423b87c633fdaa4f2de535de0d588ef34
Brian Shin <brianshin100@yahoo.com>
2016-01-12 19:53:58 -0500

    Add Drivetrain methods.

---
+++ src/org/robockets/stronghold/robot/subsystems/Drivetrain.java
@@ -0,0 +1,29 @@
+package org.robockets.stronghold.robot.subsystems;
+
+import org.robockets.stronghold.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Drivetrain extends Subsystem {
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    public void driveTank(double leftValue, double rightValue) {
+    	RobotMap.robotDrive.tankDrive(leftValue, rightValue);
+    }
+    
+    public void driveArcade(double moveValue, double rotateValue) {
+    	RobotMap.robotDrive.arcadeDrive(moveValue, rotateValue);
+    }
+    
+    public void stop() {
+    	driveTank(0, 0);
+    }
+}
+
