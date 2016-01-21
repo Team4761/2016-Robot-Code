5c5770313cc382266328e18061295c7aea65f675
Brian Shin <brianshin100@yahoo.com>
2016-01-20 19:32:48 -0500

    Add end and interrupted implementation.

--- src/org/robockets/stronghold/robot/drivetrain/Joyride.java
+++ src/org/robockets/stronghold/robot/drivetrain/Joyride.java
@@ -49,10 +49,12 @@ public class Joyride extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.driveTrain.stop();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	end();
     }
 }
