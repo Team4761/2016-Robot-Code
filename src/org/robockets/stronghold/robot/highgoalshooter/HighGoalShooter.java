1ddbf4a4bab2997c4b2a7e625c4e68312cd925b5
Mathias <mathi4semail@gmail.com>
2016-01-23 14:11:11 -0500

    Add highgoal subsystem and relevant hardware.
    Added hardware for jeff rollers, shooting mechanism, and turntable motors.

---
+++ src/org/robockets/stronghold/robot/highgoalshooter/HighGoalShooter.java
@@ -0,0 +1,41 @@
+package org.robockets.stronghold.robot.highgoalshooter;
+
+import org.robockets.stronghold.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * Subsystem for the high goal shooter mechanism including the turn table, the rollers, the shooting rollers, and the hood.
+ */
+public class HighGoalShooter extends Subsystem {
+
+    public void initDefaultCommand() {
+    }
+    
+    
+    /**
+     * Roll the Jeff rollers (the motors that guide the ball into the firing mechanism) at a desired speed.
+     * @param speed 	The speed to set both Jeff rollers at.
+     */
+    public void spinJeffRollers(double speed) {
+    	RobotMap.jeffRoller1.set(speed);
+    	RobotMap.jeffRoller2.set(speed);
+    }
+    
+    /**
+     * Roll the shooting wheel to fire the cannonball at a desired speed.
+     * @param speed 	The speed to set the shooting mechanism at.
+     */
+    public void spinShootingWheel(double speed) {
+    	RobotMap.shootingWheelMotor.set(speed);
+    }
+    
+    /**
+     * Spin the turn-table mechanism at a desired speed.
+     * @param speed		The speed to set the turn-table mechanism at to turn.
+     */
+    public void spinTurnTable(double speed) {
+    	RobotMap.turnTableMotor.set(speed);
+    }
+}
+
