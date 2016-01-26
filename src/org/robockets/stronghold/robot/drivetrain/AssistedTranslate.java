dd8cbfc4d3d2844f93f8a742037f566784247cfa
Jake Kinsella <jake.kinsella@gmail.com>
2016-01-25 20:47:07 -0500

    Take out useless angle arg to driveAssisted and create drive assisted command and type

---
+++ src/org/robockets/stronghold/robot/drivetrain/AssistedTranslate.java
@@ -0,0 +1,62 @@
+package org.robockets.stronghold.robot.drivetrain;
+
+import org.robockets.stronghold.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class AssistedTranslate extends Command {
+	private AssistedTranslateType pidType;
+	private double relativeAngle;
+
+    public AssistedTranslate() {
+    	requires(Robot.driveTrain);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize(AssistedTranslateType pidType, double relativeAngle) {
+    	this.pidType = pidType;
+    	this.relativeAngle = relativeAngle;
+    	
+    	if (pidType == AssistedTranslateType.COMPASS) {
+    		Robot.driveTrain.enableCompassPID();
+    		Robot.driveTrain.setAngle(relativeAngle, true);
+    	} else if (pidType == AssistedTranslateType.GYRO) {
+    		Robot.driveTrain.enableGyroPID();
+    		Robot.driveTrain.setAngle(relativeAngle, false);
+    	}
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	if (pidType == AssistedTranslateType.COMPASS) {
+    		Robot.driveTrain.driveAssisted(0, true);
+    	} else if (pidType == AssistedTranslateType.GYRO) {
+    		Robot.driveTrain.driveAssisted(0, false);
+    	}
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+    	if (pidType == AssistedTranslateType.COMPASS) {
+    		return Robot.driveTrain.compassPID.onTarget();
+    	} else if (pidType == AssistedTranslateType.GYRO) {
+    		return Robot.driveTrain.gyroPID.onTarget();
+    	} else { // WHAT?!
+    		return true;
+    	}
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.driveTrain.stop();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+}
---
+++ src/org/robockets/stronghold/robot/drivetrain/AssistedTranslateType.java
@@ -0,0 +1,6 @@
+package org.robockets.stronghold.robot.drivetrain;
+
+public enum AssistedTranslateType {
+	COMPASS,
+	GYRO;
+}
