56d7120683ab8a477a1956b2fe4a1bfab961b351
Mathias <mathi4semail@gmail.com>
2016-02-02 19:24:09 -0500

    Fix FireShooter command.
    Removed unneeded fluff with comments and added Robot.shooter as a requirement.

--- src/org/robockets/stronghold/robot/highgoalshooter/FireShooter.java
+++ src/org/robockets/stronghold/robot/highgoalshooter/FireShooter.java
@@ -9,32 +9,25 @@ import org.robockets.stronghold.robot.Robot;
 public class FireShooter extends Command {
 
     public FireShooter() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
+        requires(Robot.shooter);
     }
 
-    // Called just before this Command runs the first time
     protected void initialize() {
     	setTimeout(0.7);
     }
 
-    // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	Robot.shooter.spinJeffRollers(1);
     }
 
-    // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
         return isTimedOut();
     }
 
-    // Called once after isFinished returns true
     protected void end() {
     	Robot.shooter.spinJeffRollers(0);
     }
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
     protected void interrupted() {
     	end();
     }
