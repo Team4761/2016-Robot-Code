6d18f316ec2b66831ea4447b8acc8aabee706856
Jake Backer <jbacker42@gmail.com>
2016-02-08 12:59:23 -0500

    If time is 0, then stop when button is no longer clicked

--- src/org/robockets/stronghold/robot/intake/ManualSpinIntake.java
+++ src/org/robockets/stronghold/robot/intake/ManualSpinIntake.java
@@ -61,7 +61,11 @@ public class ManualSpinIntake extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-    	return isTimedOut();
+    	if(time == 0) {
+    		return false;
+    	} else {
+    		return isTimedOut();
+    	}
     }
 
     // Called once after isFinished returns true
