3d947e82dd4f6c6299992d936ba33f6d197173ec
Mathias <mathi4semail@gmail.com>
2016-02-09 15:52:29 -0500

    Revise SetShootingWheel command.
    Revise the command to be more useful for testing.

--- src/org/robockets/stronghold/robot/highgoalshooter/SetShootingWheel.java
+++ src/org/robockets/stronghold/robot/highgoalshooter/SetShootingWheel.java
@@ -6,38 +6,32 @@ import org.robockets.stronghold.robot.RobotMap;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
- *
+ * Set the shooting wheel at a desired speed.
  */
 public class SetShootingWheel extends Command {
 
-    public SetShootingWheel() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
+	double speed;
+	
+    public SetShootingWheel(double speed) {
     	requires(Robot.shooter);
+    	this.speed = speed;
     }
 
-    // Called just before this Command runs the first time
     protected void initialize() {
+    	Robot.shooter.setShootingWheelSpeed(speed);
     }
 
-    // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	//Robot.shooter.setShootingWheelSpeed(1000);
-    	RobotMap.shootingWheelMotor.pidWrite(0.2);
-    	System.out.println(RobotMap.shootingWheelMotor.pidGet());
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return Robot.shooter.shootingWheelOnTarget();
     }
 
-    // Called once after isFinished returns true
     protected void end() {
     }
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
     protected void interrupted() {
     }
 }
