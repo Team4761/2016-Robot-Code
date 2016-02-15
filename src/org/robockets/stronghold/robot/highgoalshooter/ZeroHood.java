a590500b582ba214bb15f1f2407a8818fd2c7a58
Jake Kinsella <jake.kinsella@gmail.com>
2016-02-15 17:20:25 -0500

    Set setpoint after zeroing

--- src/org/robockets/stronghold/robot/highgoalshooter/ZeroHood.java
+++ src/org/robockets/stronghold/robot/highgoalshooter/ZeroHood.java
@@ -17,6 +17,7 @@ public class ZeroHood extends Command {
     // Called just before this Command runs the first time
     protected void initialize() {
     	RobotMap.hoodEncoder.reset();
+      Robot.shooter.hoodPidController.setSetpoint(0);
     }
 
     // Called repeatedly when this Command is scheduled to run
