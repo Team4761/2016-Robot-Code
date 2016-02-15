4770fd41b1585f728f02bb1b6ed2e02613078e41
Mathias <mathi4semail@gmail.com>
2016-02-15 18:10:16 -0500

    Give MoveTurnTable time functionality.

--- src/org/robockets/stronghold/robot/highgoalshooter/MoveTurnTable.java
+++ src/org/robockets/stronghold/robot/highgoalshooter/MoveTurnTable.java
@@ -10,6 +10,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class MoveTurnTable extends Command {
 
 	Double speed = null;
+	Double time = null;
 	
 	/**
 	 * Move the hood upwards or downwards continuously.
@@ -20,9 +21,20 @@ public class MoveTurnTable extends Command {
         speed = rate;
     }
     
+    /**
+     * Move the hood upwards or downwards for a desired time at a set speed.
+     * @param rate		The speed to move at.
+     * @param time		The time to wait until stopping the set speed.
+     */
+    public MoveTurnTable(double rate, double time) {
+        requires(Robot.shooter);
+        speed = rate;
+        this.time = time;
+    }
+    
     // Called just before this Command runs the first time
     protected void initialize() {
-    	setTimeout(10);
+    	if(time!=null) setTimeout(time);
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -32,7 +44,8 @@ public class MoveTurnTable extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return isTimedOut();
+    	if(time != null) return isTimedOut(); // I didn't know what isTimedOut would return instead.
+    	else return false;
     }
 
     // Called once after isFinished returns true
