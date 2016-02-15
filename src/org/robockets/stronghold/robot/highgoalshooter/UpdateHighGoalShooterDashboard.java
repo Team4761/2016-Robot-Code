095e0a5e06784a0050ceb5993ed92e2f225220fd
Mathias <mathi4semail@gmail.com>
2016-02-15 17:23:40 -0500

    Fix petty error in dashboard.

--- src/org/robockets/stronghold/robot/highgoalshooter/UpdateHighGoalShooterDashboard.java
+++ src/org/robockets/stronghold/robot/highgoalshooter/UpdateHighGoalShooterDashboard.java
@@ -1,5 +1,6 @@
 package org.robockets.stronghold.robot.highgoalshooter;
 
+import org.robockets.stronghold.robot.EncoderPIDSource;
 import org.robockets.stronghold.robot.Robot;
 import org.robockets.stronghold.robot.RobotMap;
 
