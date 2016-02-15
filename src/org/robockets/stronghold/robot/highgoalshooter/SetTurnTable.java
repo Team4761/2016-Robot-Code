47dee450bba5aefce120609d2f70d0c2ffc01f71
Mathias <mathi4semail@gmail.com>
2016-02-15 18:50:29 -0500

    Combine MoveTurnTable and SetTurnTable into one command.

--- src/org/robockets/stronghold/robot/highgoalshooter/SetTurnTable.java
+++ src/org/robockets/stronghold/robot/highgoalshooter/SetTurnTable.java
@@ -6,11 +6,13 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
- * Set the exact position of the hood part of the high goal shooter subsystem.
+ * Set the exact position or speed of the turn table part of the high goal shooter subsystem.
  */
 public class SetTurnTable extends Command {
 
-	double angle;
+	Double angle = null;
+	Double time = null;
+	Double speed = null;
 	
 	/**
 	 * Set the angle of the turn table part of the highgoal shooter.
@@ -20,20 +22,37 @@ public class SetTurnTable extends Command {
     	requires(Robot.shooter);
     }
     
+    /**
+     * Set the speed of the turn table part of the highgoal shooter continuously.
+     * @param rate		The speed to set the turn table at.
+     */
+    
+    /**
+     * Set the speed of the turn table part of the highgoal shooter for a specified time.
+     * @param rate		The speed to set the turn table at.
+     * @param time		The time to spin the motor for.
+     */
     protected void initialize() {
-    	Robot.shooter.setTurnTableAngle(angle);
-    	setTimeout(20);
+    	if(angle!=null) { Robot.shooter.setTurnTableAngle(angle); }
+    	if(time!=null) { setTimeout(time); }
+    	if(speed!=null) {
+    		Robot.shooter.spinTurnTable(speed);
+    		Robot.shooter.disableTurnTablePID();
+    	}
     }
 
     protected void execute() {
     }
 
     protected boolean isFinished() {
-        return Robot.shooter.turnTablePidController.onTarget() || isTimedOut();
+        if(angle!=null) { return Robot.shooter.turnTablePidController.onTarget(); }
+        if(time!=null) { return isTimedOut(); }
+        return false;
     }
 
     protected void end() {
     	Robot.shooter.spinTurnTable(0);
+    	if(speed!=null) { Robot.shooter.enableTurnTablePID(); }
     }
 
     protected void interrupted() {
