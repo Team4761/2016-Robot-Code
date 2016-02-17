73c889bfc76e85ff5a28a39bd9127b760fdf0b94
jared whitney <jaredwhitney@users.noreply.github.com>
2016-02-17 17:14:09 -0500

    Create AutoCommands.java

---
+++ src/org/robockets/stronghold/robot/AutoCommands.java
@@ -0,0 +1,60 @@
+class AutoCommands
+{
+	Defense[] defenses;
+	Action action;
+	int robotPosition;
+	Defense forwardDefense;
+	Defense returnDefense;
+	public void init()
+	{
+		defenses = new Defense[5];
+		defenses[0] = parseDefense(FieldConfiguration.defense1);
+		defenses[1] = parseDefense(FieldConfiguration.defense2);
+		defenses[2] = parseDefense(FieldConfiguration.defense3);
+		defenses[3] = parseDefense(FieldConfiguration.defense4);
+		defenses[4] = parseDefense(FieldConfiguration.defense5);
+		robotPosition = parsePosition(FieldConfiguration.robotPosition);
+		forwardDefense = defenses[robotPosition];
+		returnDefense = parseDefense(FieldConfiguration.returnDefense);
+	}
+	public void run()
+	{
+		runCommandUntilFinished(forwardDefense.forwardCommand);
+		runCommandUntilFinished(action.command);
+		runCommandUntilFinished(returnDefense.backwardCommand);
+	}
+	public void runCommandUntilFinished(Command c)
+	{
+		c.start();
+		while (!c.isFinished());
+	}
+	public Defense parseDefense(String s)
+	{
+		switch (String s)
+		{
+			case "Portcullist" :
+				return Defense.PORTICULLIS;
+			case "Cheval de Frise" :
+				return Defense.CHEVAL_DE_FRISE;
+			case "Moat" :
+				return Defense.MOAT;
+			case "Ramparts" :
+				return Defense.RAMPARTS;
+			case "DrawBridge" :
+				return Defense.DRAWBRIDGE;
+			case "Sally Port" :
+				return Defense.SALLY_PORT;
+			case "Rock Wall" :
+				return Defense.ROCK_WALL;
+			case "Rough Terrian" :
+				return Defense.ROUGH_TERRAIN;
+			case default :
+				return null;
+		}
+	}
+	public int parsePosition(String s)
+	{
+		return ((int)s.charAt(s.length()-1))-0x30;	// because it really doesn't need a switch statement...
+	}
+	
+}
