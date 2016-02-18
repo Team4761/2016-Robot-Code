ea139b218c2e7d3b2be8198cac5ebad161f2aeec
Brian Shin <brianshin100@yahoo.com>
2016-02-17 19:51:38 -0500

    Remove SmartDashboard things.

--- src/org/robockets/stronghold/robot/drivetrain/Joyride.java
+++ src/org/robockets/stronghold/robot/drivetrain/Joyride.java
@@ -21,36 +21,11 @@ public class Joyride extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-        SmartDashboard.putNumber("Encoder P", Robot.driveTrain.encoderPID.getP());
-        SmartDashboard.putNumber("Encoder I", Robot.driveTrain.encoderPID.getI());
-        SmartDashboard.putNumber("Encoder D", Robot.driveTrain.encoderPID.getD());
-        SmartDashboard.putNumber("Encoder Setpoint", Robot.driveTrain.encoderPID.getSetpoint());
-        SmartDashboard.putBoolean("Reset encoderPID", false);
-        SmartDashboard.putBoolean("Enable encoderPID", false);
-        Robot.driveTrain.encoderPID.setSetpoint(0);
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	SmartDashboard.putNumber("Encoder PID", RobotMap.driveEncoder.get());
-    	SmartDashboard.putNumber("Encoder PID Out", Robot.driveTrain.encoderPID.get());
-        System.out.println(RobotMap.driveEncoder.get()); // temp
-        Robot.driveTrain.encoderPID.setSetpoint(SmartDashboard.getNumber("Encoder Setpoint"));
-        if (!SmartDashboard.getBoolean("Enable encoderPID")) {
-            Robot.driveTrain.encoderPID.disable();
-            Robot.driveTrain.driveArcade(OI.joystick.getRawAxis(1), -OI.joystick.getRawAxis(4));
-            if (SmartDashboard.getBoolean("Reset encoderPID")) {
-            	RobotMap.driveEncoder.reset();
-            }
-        } else {
-        	Robot.driveTrain.encoderPID.setPID(
-        			SmartDashboard.getNumber("Encoder P"), 
-        			SmartDashboard.getNumber("Encoder I"), 
-        			SmartDashboard.getNumber("Encoder D"));
-            Robot.driveTrain.encoderPID.enable();
-            Robot.driveTrain.driveArcade(Robot.driveTrain.encoderPID.get(), 0);
-        }
-        
+    	Robot.driveTrain.driveArcade(OI.joystick.getRawAxis(1), -OI.joystick.getRawAxis(4));
     }
 
     // Make this return true when this Command no longer needs to run execute()
