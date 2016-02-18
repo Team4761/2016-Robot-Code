e07b6b1cad66d758235ab1838eadb5f3a62d9bc5
jakebacker <jbacker42@gmail.com>
2016-02-18 15:53:25 -0500

    Fix bad code styling

--- src/org/robockets/stronghold/robot/intake/Intake.java
+++ src/org/robockets/stronghold/robot/intake/Intake.java
@@ -4,6 +4,7 @@ import org.robockets.stronghold.robot.DummyPIDOutput;
 import org.robockets.stronghold.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -11,123 +12,82 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  *
  */
 public class Intake extends Subsystem {
-	
-	IntakeSide intakeSide;
-	
-	
-	public final PIDController encoderPIDFront = new PIDController(0.1, 0.1, 0, RobotMap.intakeEncoderFront, new DummyPIDOutput());
-	public final PIDController encoderPIDBack = new PIDController(0, 0, 0, RobotMap.intakeEncoderBack, new DummyPIDOutput());
-	
-	public Intake(IntakeSide intakeSide) {
-		this.intakeSide = intakeSide;
-		encoderPIDFront.disable();
-		encoderPIDFront.setSetpoint(0);
-		encoderPIDFront.setPercentTolerance(0.05);
-		encoderPIDFront.setContinuous(true);
-		
-		encoderPIDBack.disable();
-		encoderPIDBack.setSetpoint(0);
-		encoderPIDBack.setPercentTolerance(0.05);
-		encoderPIDBack.setContinuous(true);
-	}
-	
-    public void initDefaultCommand() {
-    	
-    }
-    
-    public void spin(double speed) {
-    	if (intakeSide == IntakeSide.FRONT) {
-    		RobotMap.intakeMotorFront.set(speed);
-    	} else {
-    		RobotMap.intakeMotorBack.set(speed);
-    	}
-    }
-    
-    public void spinIn() {
-    	if (intakeSide == IntakeSide.FRONT) {
-    		RobotMap.intakeMotorFront.set(0.5);
-    	} else {
-    		RobotMap.intakeMotorBack.set(0.5);
-    	}
-    }
-    
-    public void spinOut() {
-    	if (intakeSide == IntakeSide.FRONT) {
-    		RobotMap.intakeMotorFront.set(-0.5);
-    	} else {
-    		RobotMap.intakeMotorBack.set(-0.5);
-    	}
-    }
-    
-    public void moveUp() {
-    	if (intakeSide == IntakeSide.FRONT) {
-    		RobotMap.intakeVerticalMotorFront.set(0.5);
-    	} else {
-    		RobotMap.intakeVerticalMotorBack.set(0.5);
-    	}
-    }
-    
-    public void moveDown() {
-    	if (intakeSide == IntakeSide.FRONT) {
-    		RobotMap.intakeVerticalMotorFront.set(-0.5);
-    	} else {
-    		RobotMap.intakeVerticalMotorBack.set(-0.5);
-    	}
-    }
-    
-    public void move(int speed) {
-    	if (intakeSide == IntakeSide.FRONT) {
-    		RobotMap.intakeVerticalMotorFront.set(speed);
-    	} else {
-    		RobotMap.intakeVerticalMotorBack.set(speed);
-    	}
-    }
-    
-    public void spinAssisted() {
-    	encoderPIDFront.setPID(SmartDashboard.getNumber("P Front"), SmartDashboard.getNumber("I Front"), SmartDashboard.getNumber("D Front"));
-    	encoderPIDBack.setPID(SmartDashboard.getNumber("P Back"), SmartDashboard.getNumber("I Back"), SmartDashboard.getNumber("D Back"));
-    	
-    	if (intakeSide == IntakeSide.FRONT) {
-    		RobotMap.intakeMotorFront.set(encoderPIDFront.get());
-    	} else {
-    		RobotMap.intakeMotorBack.set(encoderPIDBack.get());
-    	}
-    }
-    
-    public void setIntakeAngle(double angle) {
-    	if (intakeSide == IntakeSide.FRONT) {
-    		encoderPIDFront.setSetpoint(angle);
-    	} else {
-    		encoderPIDBack.setSetpoint(angle);
-    	}
-    }
-    
-    public void enablePID() {
-    	if (intakeSide == IntakeSide.FRONT) {
-    		encoderPIDFront.enable();
-    		encoderPIDFront.reset();
-    		encoderPIDFront.setSetpoint(RobotMap.intakeEncoderFront.get());
-    	} else {
-    		encoderPIDBack.enable();
-    		encoderPIDBack.reset();
-    		encoderPIDBack.setSetpoint(RobotMap.intakeEncoderFront.get());
-    	}
-    }
-    
-    public void stopIntake() {
-    	if (intakeSide == IntakeSide.FRONT) {
-    		RobotMap.intakeMotorFront.set(0);
-    	} else {
-    		RobotMap.intakeMotorBack.set(0);
-    	}
-    }
-    
-    public void stopVertical() {
-    	if (intakeSide == IntakeSide.FRONT) {
-    		RobotMap.intakeVerticalMotorFront.set(0);
-    	} else {
-    		RobotMap.intakeVerticalMotorBack.set(0);
-    	}
-    }
-}
 
+	Victor intakeVerticalMotor;
+	Victor intakeMotor;
+
+	public final PIDController encoderPID;
+
+	public Intake(IntakeSide intakeSide) {
+		if (intakeSide == IntakeSide.FRONT) {
+			intakeVerticalMotor = RobotMap.intakeVerticalMotorFront;
+			intakeMotor = RobotMap.intakeMotorFront;
+			encoderPID = new PIDController(0.1, 0.1, 0, RobotMap.intakeEncoderFront, new DummyPIDOutput());
+		} else {
+			intakeVerticalMotor = RobotMap.intakeVerticalMotorBack;
+			intakeMotor = RobotMap.intakeMotorBack;
+			encoderPID = new PIDController(0.1, 0.1, 0, RobotMap.intakeEncoderBack, new DummyPIDOutput());
+		}
+
+		encoderPID.disable();
+		encoderPID.setSetpoint(0);
+		encoderPID.setPercentTolerance(0.05);
+		encoderPID.setContinuous(true);
+
+		encoderPID.disable();
+		encoderPID.setSetpoint(0);
+		encoderPID.setPercentTolerance(0.05);
+		encoderPID.setContinuous(true);
+	}
+
+	public void initDefaultCommand() {
+
+	}
+
+	public void spin(double speed) {
+		intakeMotor.set(speed);
+	}
+
+	public void spinIn() {
+		intakeMotor.set(0.5);
+	}
+
+	public void spinOut() {
+		intakeMotor.set(-0.5);
+	}
+
+	public void moveUp() {
+		intakeVerticalMotor.set(0.5);
+	}
+
+	public void moveDown() {
+		intakeVerticalMotor.set(-0.5);
+	}
+
+	public void move(int speed) {
+		intakeVerticalMotor.set(speed);
+	}
+
+	public void spinAssisted() {
+		encoderPID.setPID(SmartDashboard.getNumber("P"), SmartDashboard.getNumber("I"), SmartDashboard.getNumber("D")); // Still broken
+		intakeMotor.set(encoderPID.get());
+	}
+
+	public void setIntakeAngle(double angle) {
+		encoderPID.setSetpoint(angle);
+	}
+
+	public void enablePID() {
+		encoderPID.enable();
+		encoderPID.reset();
+		encoderPID.setSetpoint(encoderPID.getSetpoint());
+	}
+
+	public void stopIntake() {
+		intakeMotor.set(0);
+	}
+
+	public void stopVertical() {
+		intakeVerticalMotor.set(0);
+	}
+}
--- src/org/robockets/stronghold/robot/intake/ManualVerticalIntake.java
+++ src/org/robockets/stronghold/robot/intake/ManualVerticalIntake.java
@@ -8,90 +8,78 @@ import edu.wpi.first.wpilibj.command.Command;
  * A Command to spin the intake motor forward or backward
  */
 public class ManualVerticalIntake extends Command {
-	
-	public Direction direction; //Object for the Direction enum
-	public IntakeSide intakeSide;
-	
-	int time; //Used to set timeout
-	
-	int speed; //Used to set speed manually
-	
+
+	public Direction direction; // Object for the Direction enum
+	Intake intake;
+
+	int time; // Used to set timeout
+
+	int speed; // Used to set speed manually
+
 	/**
 	 * Initalizes some variables
 	 * 
-	 * @param direction  Used to initalize Direction enum
-	 * @param time  Takes input for time
-	 * */
-    public ManualVerticalIntake(Direction direction, int time, IntakeSide intakeSide) {
-    	if (intakeSide == IntakeSide.FRONT) {
-    		requires(Robot.intakeFront);
-    	} else {
-    		requires(Robot.intakeBack);
-    	}
-    	this.direction = direction;
-    	this.intakeSide = intakeSide;
-    	this.time = time;
-    }
-    
-    public ManualVerticalIntake(int speed, int time, IntakeSide intakeSide) {
-    	if (intakeSide == IntakeSide.FRONT) {
-    		requires(Robot.intakeFront);
-    	} else {
-    		requires(Robot.intakeBack);
-    	}
-    	this.speed = speed;
-    	this.direction = Direction.MANUAL;
-    	this.intakeSide = intakeSide;
-    	this.time = time;
-    }
+	 * @param direction
+	 *            Used to initalize Direction enum
+	 * @param time
+	 *            Takes input for time
+	 */
+	public ManualVerticalIntake(Direction direction, int time, IntakeSide intakeSide) {
+		if (intakeSide == IntakeSide.FRONT) {
+			requires(Robot.intakeFront);
+			intake = Robot.intakeFront;
+		} else {
+			requires(Robot.intakeBack);
+			intake = Robot.intakeBack;
+		}
+		this.direction = direction;
+		this.time = time;
+	}
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    	setTimeout(time);
-    }
+	public ManualVerticalIntake(int speed, int time, IntakeSide intakeSide) {
+		if (intakeSide == IntakeSide.FRONT) {
+			requires(Robot.intakeFront);
+			intake = Robot.intakeFront;
+		} else {
+			requires(Robot.intakeBack);
+			intake = Robot.intakeBack;
+		}
+		this.speed = speed;
+		this.direction = Direction.MANUAL;
+		this.time = time;
+	}
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    	if (intakeSide == IntakeSide.FRONT) {
-    		if (direction == Direction.UP) {
-    			Robot.intakeFront.moveUp();
-    		} else if (direction == Direction.DOWN){
-    			Robot.intakeFront.moveDown();
-    		} else if (direction == Direction.DOWN){
-    			Robot.intakeFront.move(speed);
-    		} else {
-    			Robot.intakeFront.stopVertical();
-    		}
-    	} else {
-    		if (direction == Direction.UP) {
-    			Robot.intakeBack.moveUp();
-    		} else if (direction == Direction.DOWN){
-    			Robot.intakeBack.moveDown();
-    		} else if (direction == Direction.DOWN){
-    			Robot.intakeBack.move(speed);
-    		} else {
-    			Robot.intakeBack.stopVertical();
-    		}
-    	}
-    }
+	// Called just before this Command runs the first time
+	protected void initialize() {
+		setTimeout(time);
+	}
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-    	return isTimedOut();
-    }
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		if (direction == Direction.UP) {
+			intake.moveUp();
+		} else if (direction == Direction.DOWN) {
+			intake.moveDown();
+		} else if (direction == Direction.MANUAL) {
+			intake.move(speed);
+		} else {
+			intake.stopVertical();
+		}
+	}
 
-    // Called once after isFinished returns true
-    protected void end() {
-    	if (intakeSide == IntakeSide.FRONT) {
-    		Robot.intakeFront.stopVertical();
-    	} else {
-    		Robot.intakeBack.stopVertical();
-    	}
-    }
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return isTimedOut();
+	}
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    	end();
-    }
+	// Called once after isFinished returns true
+	protected void end() {
+		intake.stopVertical();
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+		end();
+	}
 }
