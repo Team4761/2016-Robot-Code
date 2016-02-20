ba5138d2e5e46fc5be247b0bd42ab75f556aab3b
Mathias <mathi4semail@gmail.com>
2016-02-20 09:56:21 -0500

    Refactor vertical aiming command.

--- src/org/robockets/stronghold/robot/highgoalshooter/FreeFire.java
+++ src/org/robockets/stronghold/robot/highgoalshooter/FreeFire.java
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class FreeFire extends CommandGroup {
     
     public  FreeFire() {
-    	addSequential(new AimShooter());
+    	addSequential(new VerticalAlign());
     	addSequential(new FireShooter());
     	addSequential(new MoveShootingWheel(0));
     }
