8b564e59e6a1ddac2505ed7580832bb9d2696667
Jake Kinsella <jake.kinsella@gmail.com>
2016-02-20 09:50:47 -0500

    Split up the auto shoot high command into another command that positions the robot infront of the middle goal

--- src/org/robockets/stronghold/robot/autonomous/AutoShootHigh.java
+++ src/org/robockets/stronghold/robot/autonomous/AutoShootHigh.java
@@ -15,34 +15,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class AutoShootHigh extends CommandGroup {
     
     public  AutoShootHigh(int defense) {
-        Command turnCommand = null;
-        Command translateCommand = null;
-
-        switch (defense) {
-	        case 1:
-		        turnCommand = new AssistedDrive(AssistedRotateType.GYRO, 45);
-		        translateCommand = new AssistedDrive(AssistedTranslateType.ENCODER, AssistedRotateType.ENCODER, 5, Robot.driveTrain.getEncodersOffset());
-		        break;
-	        case 2:
-		        turnCommand = new AssistedDrive(AssistedRotateType.GYRO, 30);
-		        translateCommand = new AssistedDrive(AssistedTranslateType.ENCODER, AssistedRotateType.ENCODER, 3, Robot.driveTrain.getEncodersOffset());
-		        break;
-	        case 3:
-		        turnCommand = new AssistedDrive(AssistedRotateType.GYRO, 0);
-		        translateCommand = new AssistedDrive(AssistedTranslateType.ENCODER, AssistedRotateType.ENCODER, 1, Robot.driveTrain.getEncodersOffset());
-		        break;
-	        case 4:
-		        turnCommand = new AssistedDrive(AssistedRotateType.GYRO, -30);
-		        translateCommand = new AssistedDrive(AssistedTranslateType.ENCODER, AssistedRotateType.ENCODER, 3, Robot.driveTrain.getEncodersOffset());
-		        break;
-	        case 5:
-	        	turnCommand = new AssistedDrive(AssistedRotateType.GYRO, -45);
-		        translateCommand = new AssistedDrive(AssistedTranslateType.ENCODER, AssistedRotateType.ENCODER, 5, Robot.driveTrain.getEncodersOffset());
-	        	break;
-        }
-        
-        addSequential(turnCommand);
-        addSequential(translateCommand);
+        addSequential(new MoveToMiddleGoal(defense));
         addSequential(new FreeFire());
     }
 }
