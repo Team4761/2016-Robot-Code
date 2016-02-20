02e29305f5404119d43e55a2947d3543baa8453b
Junaid Baig <junaidb790@gmail.com>
2016-02-20 10:54:02 -0500

    Add SallyPort to autonomus.

---
+++ src/org/robockets/stronghold/robot/commands/SallyPort.java
@@ -0,0 +1,12 @@
+package org.robockets.stronghold.robot.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import org.robockets.stronghold.robot.drivetrain.AssistedDrive;
+import org.robockets.stronghold.robot.drivetrain.AssistedRotateType;
+import org.robockets.stronghold.robot.drivetrain.AssistedTranslateType;
+
+public class SallyPort extends CommandGroup {
+	public SallyPort() {
+		addSequential(new AssistedDrive(AssistedTranslateType.ENCODER, AssistedRotateType.ENCODER, 1, 0 ));
+	}
+}
