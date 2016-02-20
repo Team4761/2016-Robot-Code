906f8e2f0215b32c3da8440f6860e3f0dfc00709
Jake Kinsella <jake.kinsella@gmail.com>
2016-02-20 11:32:44 -0500

    Update PWM ports

--- src/org/robockets/stronghold/robot/RobotMap.java
+++ src/org/robockets/stronghold/robot/RobotMap.java
@@ -20,28 +20,21 @@ public class RobotMap {
 	private static SerialPort navXSerialPort = new SerialPort(57600, SerialPort.Port.kMXP);
 	private static byte updateRateHz = 50;
 	public static IMUAdvanced navX = new IMUAdvanced(navXSerialPort, updateRateHz);
-	public static Victor intakeMotorFront = new Victor(3); //TEMP
-	public static Victor intakeMotorBack = new Victor(13); //TEMP
+	public static Victor intakeMotorFront = new Victor(3);
+	public static Victor intakeMotorBack = new Victor(1);
 	public static Encoder intakeEncoderFront = new Encoder(3, 4);
 	public static Encoder intakeEncoderBack = new Encoder(7, 8);
-	public static RobotDrive robotDrive = new RobotDrive(0, 1);
-	public static Encoder intakeEncoder = new Encoder(5, 6);
-	public static Victor intakeRollerMotor = new Victor(2);
-	public static Victor intakeVerticalMotor = new Victor(3); //TEMP
-	//public static Encoder turnTableEncoder = new Encoder(3, 4);
+	public static RobotDrive robotDrive = new RobotDrive(18, 19);
 	public static Encoder turnTableEncoder = new Encoder(4, 9);
-	public static Victor intakeVerticalMotorFront = new Victor(2); //TEMP
-	public static Victor intakeVerticalMotorBack = new Victor(9); //TEMP
-	public static Victor jeffRoller1 = new Victor(4); // TEMP
-	public static Victor jeffRoller2 = new Victor(5); // TEMP
-	public static Victor turnTableMotor = new Victor(5); // TEMP
-	public static Victor hoodMotor = new Victor(8); // TEMP
-	//public static Encoder hoodEncoder = new Encoder(0, 1);
+	public static Victor intakeVerticalMotorFront = new Victor(2);
+	public static Victor intakeVerticalMotorBack = new Victor(0);
+	public static Victor turnTableMotor = new Victor(6);
+	public static Victor hoodMotor = new Victor(7);
 	public static Encoder hoodEncoder = new Encoder(7, 8);
 	public static Encoder driveEncoder = new Encoder(0, 1);
 	public static Encoder driveEncoder2 = new Encoder(2, 3);
 	public static CANTalon shootingWheelMotor = new CANTalon(2);
-	public static Servo shootingFlipper = new Servo(4);
+	public static Servo shootingFlipper = new Servo(6);
 
 	
 	public RobotMap () {
