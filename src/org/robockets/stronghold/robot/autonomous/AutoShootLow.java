6d3b9e784da837ad8b194e228411cf23be918ec9
Jake Kinsella <jake.kinsella@gmail.com>
2016-02-21 10:03:51 -0500

    Merge SetIntake into SetVertical intake and cleanup bad code

--- src/org/robockets/stronghold/robot/autonomous/AutoShootLow.java
+++ src/org/robockets/stronghold/robot/autonomous/AutoShootLow.java
@@ -5,7 +5,7 @@ import org.robockets.stronghold.robot.drivetrain.AssistedDrive;
 import org.robockets.stronghold.robot.drivetrain.AssistedRotateType;
 import org.robockets.stronghold.robot.drivetrain.AssistedTranslateType;
 import org.robockets.stronghold.robot.intake.IntakeSide;
-import org.robockets.stronghold.robot.intake.ManualSpinIntake;
+import org.robockets.stronghold.robot.intake.SpinIntake;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
@@ -17,7 +17,7 @@ public class AutoShootLow extends CommandGroup {
     public  AutoShootLow(int defense) {
     	addSequential(new MoveToMiddleGoal(defense));
         addSequential(new AssistedDrive(AssistedTranslateType.ENCODER, AssistedRotateType.ENCODER, 24, 0));
-        addSequential(new ManualSpinIntake(Direction.FORWARD, 3, IntakeSide.FRONT));
+        addSequential(new SpinIntake(Direction.FORWARD, 3, IntakeSide.FRONT));
         addSequential(new AssistedDrive(AssistedTranslateType.ENCODER, AssistedRotateType.ENCODER, -24, 0));
     }
 }
