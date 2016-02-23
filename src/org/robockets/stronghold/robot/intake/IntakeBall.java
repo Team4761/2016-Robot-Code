18ec3d4a47165e220fede6d47f9054c0b16c5bbc
Jake Kinsella <jake.kinsella@gmail.com>
2016-02-22 23:03:04 -0500

    Update commands to be more tuned

--- src/org/robockets/stronghold/robot/intake/IntakeBall.java
+++ src/org/robockets/stronghold/robot/intake/IntakeBall.java
@@ -12,7 +12,7 @@ public class IntakeBall extends CommandGroup {
     public IntakeBall(IntakeSide intakeSide) {
         addSequential(new SetVerticalIntake(0, intakeSide)); // Ideal position is 0 degrees with the bottom of arm litle more than parallel to ground
         addSequential(new CheckIntakeBreakBeam(intakeSide)); // Spins until break break sensor is broken
-        addSequential(new SetVerticalIntake(90, intakeSide)); // Up
+        addSequential(new SetVerticalIntake(-90, intakeSide)); // Up
         addSequential(new SpinIntake(Direction.BACKWARD, 5, intakeSide)); // 5 seconds I guess
     }
 }
