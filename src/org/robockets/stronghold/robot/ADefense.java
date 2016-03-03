4a55ef02a330441c4ba0f3fa483cd12a53a0fdb9
jared whitney <jaredwhitney@users.noreply.github.com>
2016-03-02 20:31:15 -0500

    Update ADefense.java

--- src/org/robockets/stronghold/robot/ADefense.java
+++ src/org/robockets/stronghold/robot/ADefense.java
@@ -3,15 +3,15 @@ import edu.wpi.first.wpilibj.command.Command;
 public enum ADefense
 {
 	
-	public PORTCULLIS ("AutoPortcullisForward", "AutoPortcullisBackward"),
-	public CHEVAL_DE_FRISE ("AutoChevalDeFriseForward", "AutoChevalDeFriseBackward"),
-	public RAMPARTS ("AutoRampartsForward", "AutoRampartsBackward"),
-	public MOAT ("AutoMoatForward", "AutoMoatBackward"),
-	public DRAWBRIDGE ("AutoDrawbridgeForward", "AutoDrawbridgeBackward"),
-	public SALLY_PORT ("AutoSallyPortForward", "AutoSallyPortBackward"),
-	public ROCK_WALL ("AutoRockWallForward", "AutoRockWallBackward"),
-	public ROUGH_TERRAIN ("AutoRoughTerrainForward", "AutoRoughTerrainBackward"),
-	public LOW_BAR	("AutoLowBarForward", "AutoLowBarBackward");
+	PORTCULLIS ("AutoPortcullisForward", "AutoPortcullisBackward"),
+	CHEVAL_DE_FRISE ("AutoChevalDeFriseForward", "AutoChevalDeFriseBackward"),
+	RAMPARTS ("AutoRampartsForward", "AutoRampartsBackward"),
+	MOAT ("AutoMoatForward", "AutoMoatBackward"),
+	DRAWBRIDGE ("AutoDrawbridgeForward", "AutoDrawbridgeBackward"),
+	SALLY_PORT ("AutoSallyPortForward", "AutoSallyPortBackward"),
+	ROCK_WALL ("AutoRockWallForward", "AutoRockWallBackward"),
+	ROUGH_TERRAIN ("AutoRoughTerrainForward", "AutoRoughTerrainBackward"),
+	LOW_BAR	("AutoLowBarForward", "AutoLowBarBackward");
 	
 	final Command forwardCommand, backwardCommand;
 	
