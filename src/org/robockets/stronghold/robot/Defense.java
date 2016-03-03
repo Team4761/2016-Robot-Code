589fb89d0ce00756e509e0dcaa34cd2a9e6d7cbd
jared whitney <jaredwhitney@users.noreply.github.com>
2016-03-02 19:54:18 -0500

    Create Defense.java

---
+++ src/org/robockets/stronghold/robot/Defense.java
@@ -0,0 +1,27 @@
+enum Defense
+{
+	
+	PORTCULLIS ("AutoPortcullisForward", "AutoPortcullisBackward"),
+	CHEVAL_DE_FRISE ("AutoChevalDeFriseForward", "AutoChevalDeFriseBackward"),
+	RAMPARTS ("AutoRampartsForward", "AutoRampartsBackward"),
+	MOAT ("AutoMoatForward", "AutoMoatBackward"),
+	DRAWBRIDGE ("AutoDrawbridgeForward", "AutoDrawbridgeBackward"),
+	SALLY_PORT ("AutoSallyPortForward", "AutoSallyPortBackward"),
+	ROCK_WALL ("AutoRockWallForward", "AutoRockWallBackward"),
+	ROUGH_TERRAIN ("AutoRoughTerrainForward", "AutoRoughTerrainBackward"),
+	LOW_BAR	("AutoLowBarForward", "AutoLowBarBackward");
+	
+	final Command forwardCommand, backwardCommand;
+	
+	Defense (String forwardCommandClassName, String backwardCommandClassName)
+	{
+		forwardCommand = getCommandInstanceByName(forwardCommandClassName);
+		backwardCommand = getCommandInstanceByName(backwardCommandClassName);
+	}
+	
+	Command getCommandInstanceByName(String str)
+	{
+		return (Command)Class.forName("org.robockets.stronghold.robot.commands." + str).getConstructor(null).newInstance(new Object[0]);
+	}
+}
+
