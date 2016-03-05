2a37fbe0c2b2037008dac667d6987508252e4d16
Jake Kinsella <jake.kinsella@gmail.com>
2016-03-05 13:00:32 -0500

    Add javadocs for cleat shot

--- src/org/robockets/stronghold/robot/highgoalshooter/AimCleatShot.java
+++ src/org/robockets/stronghold/robot/highgoalshooter/AimCleatShot.java
@@ -6,7 +6,7 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /**
- *
+ * Aim the shooter towards the cleat
  */
 public class AimCleatShot extends CommandGroup {
     
--- src/org/robockets/stronghold/robot/highgoalshooter/FireCleatShot.java
+++ src/org/robockets/stronghold/robot/highgoalshooter/FireCleatShot.java
@@ -3,7 +3,7 @@ package org.robockets.stronghold.robot.highgoalshooter;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /**
- *
+ * Fire the shooter for a cleat shot. To be run after AimCleatShot
  */
 public class FireCleatShot extends CommandGroup {
     
