eb3a037133d27dffb6b136214743b994c3f04099
Jake Kinsella <jake.kinsella@gmail.com>
2016-03-06 16:00:41 -0500

    Update cleat RPM

--- src/org/robockets/stronghold/robot/highgoalshooter/FireCleatShot.java
+++ src/org/robockets/stronghold/robot/highgoalshooter/FireCleatShot.java
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class FireCleatShot extends CommandGroup {
     
     public  FireCleatShot() {
-        addSequential(new MoveShootingWheel(1150));
+        addSequential(new MoveShootingWheel(1160));
         addSequential(new FireShooter());
     }
 }
