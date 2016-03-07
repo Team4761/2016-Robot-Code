b6ff2b22dad5e01d1f513dbc8311056df62e0610
Brian Shin <brianshin100@yahoo.com>
2016-03-06 21:55:07 -0500

    Last fix(i hope)

--- src/org/robockets/stronghold/robot/autonomous/SallyPort.java
+++ src/org/robockets/stronghold/robot/autonomous/SallyPort.java
@@ -9,10 +9,10 @@ import org.robockets.stronghold.robot.drivetrain.TurnRelative;
 
 
 public class SallyPort extends CommandGroup {
-	
 	public SallyPort() {
 		addSequential(new AssistedDrive(AssistedTranslateType.ENCODER, AssistedRotateType.ENCODER, 1, 0 ));
 
 		//addSequential(new TurnRelative(50));
+
 	}
 }
