64796ae8af00df31846fcc9d64c580fc1c8d1ff4
Jake Backer <jbacker42@gmail.com>
2016-03-09 19:32:20 -0500

    Remove unused imports in SallyPort.java

--- src/org/robockets/stronghold/robot/autonomous/SallyPort.java
+++ src/org/robockets/stronghold/robot/autonomous/SallyPort.java
@@ -5,9 +5,6 @@ import org.robockets.stronghold.robot.drivetrain.AssistedDrive;
 import org.robockets.stronghold.robot.drivetrain.AssistedRotateType;
 import org.robockets.stronghold.robot.drivetrain.AssistedTranslateType;
 
-import org.robockets.stronghold.robot.drivetrain.TurnRelative;
-
-
 public class SallyPort extends CommandGroup {
 	public SallyPort() {
 		addSequential(new AssistedDrive(AssistedTranslateType.ENCODER, AssistedRotateType.ENCODER, 1, 0 ));
