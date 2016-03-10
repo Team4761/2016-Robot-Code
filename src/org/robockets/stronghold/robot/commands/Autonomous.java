2836b392127553cee9524186af1067a4a39d2338
Jake Backer <jbacker42@gmail.com>
2016-03-09 19:30:05 -0500

    Remove unused imports in Autonomous.java

--- src/org/robockets/stronghold/robot/commands/Autonomous.java
+++ src/org/robockets/stronghold/robot/commands/Autonomous.java
@@ -1,11 +1,9 @@
 package org.robockets.stronghold.robot.commands;
 
 import org.robockets.stronghold.robot.Direction;
-import org.robockets.stronghold.robot.Robot;
 import org.robockets.stronghold.robot.autonomous.LowBar;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * Run commands during autonomous.
