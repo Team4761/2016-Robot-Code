685180bbd5488262e6b2f7ea16877a8485ec6d28
Mathias <mathi4semail@gmail.com>
2016-03-21 21:09:10 -0400

    Reimplement heartbeat and write a test case for PID.

---
+++ src/org/robockets/stronghold/robot/commands/VisionHorizontalTest.java
@@ -0,0 +1,19 @@
+package org.robockets.stronghold.robot.commands;
+
+import org.robockets.stronghold.robot.turntable.MoveTurnTable;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class VisionHorizontalTest extends CommandGroup {
+    
+    public  VisionHorizontalTest() {
+    	addSequential(new MoveTurnTable(0));
+    	addSequential(new MoveTurnTable(180));
+    	addSequential(new MoveTurnTable(0));
+    	addSequential(new MoveTurnTable(30));
+    	addSequential(new VisionEquationOutput(55/1024)); // Or something like that.
+    }
+}
--- src/org/robockets/stronghold/robot/turntable/HorizontalAlign.java
+++ src/org/robockets/stronghold/robot/turntable/HorizontalAlign.java
@@ -15,61 +15,73 @@ public class HorizontalAlign extends Command {
 	NetworkTable table;
 	boolean continuous;
 	boolean onTargetForReal = false;
-	
+
 	/**
 	 * * @param continuous		If it should stop when on target.
 	 */
-    public HorizontalAlign(boolean continuous) {
-    	requires(Robot.turntable);
-    	this.continuous = continuous;
-    }
+	public HorizontalAlign(boolean continuous) {
+		requires(Robot.turntable);
+		this.continuous = continuous;
+	}
 
-    protected void initialize() {
-    	table = NetworkTable.getTable("vision");
-    	table.putNumber("heartbeat", 1);
-    	onTargetForReal = false;
-    	SmartDashboard.putBoolean("Shoot Horizontally Aligned", false);
-    }
+	protected void initialize() {
+		table = NetworkTable.getTable("vision");
+		table.putNumber("heartbeat", 1);
+		onTargetForReal = false;
+		SmartDashboard.putBoolean("Shoot Horizontally Aligned", false);
+	}
 
-    protected void execute() {
-    	double pixelError = table.getNumber("horiz_offset", 0);
-    	 SmartDashboard.putNumber("factorz", SmartDashboard.getNumber("factorz", 0.0305));
-    	double factor = SmartDashboard.getNumber("factorz", 0.0305);
-    	
-    	double output = Robot.turntable.getAngle() + (factor * pixelError);
-    	Robot.turntable.setAngle(output);
-    	SmartDashboard.putNumber("output for turntable", output);
-    	SmartDashboard.putNumber("pid error", Robot.turntable.getSetpoint() - RobotMap.turntablePIDSource.pidGet());
-    	Robot.turntable.setAngle(output);
-    			
-    	if (!continuous && Robot.turntable.onTarget()) {
-    		if (!continuous && Math.abs(pixelError) < 20) {
-    			if (!onTargetForReal) {
-    				setTimeout(1);
-    			}
-    		
-    			onTargetForReal = true;
-    		} else {
-    			onTargetForReal = false;
-    		}
-    	}
-    }
-    	
-    protected boolean isFinished() {    	
-    	if (Robot.turntable.onTarget() && isTimedOut() && onTargetForReal) {
-    		SmartDashboard.putBoolean("Shoot Horizontally Aligned", true);
-    		if (continuous == false) { return true; }
-    	} else {
-    		SmartDashboard.putBoolean("Shoot Horizontally Aligned", false);
-    	}
-    	return false;
-    }
+	protected void execute() {
+		double pixelError = table.getNumber("horiz_offset", 0);
+		SmartDashboard.putNumber("factorz", SmartDashboard.getNumber("factorz", 0.0305));
+		double factor = SmartDashboard.getNumber("factorz", 53/1204);
 
-    protected void end() {
-    	SmartDashboard.putBoolean("Shoot Horizontally Aligned", false);
-    }
+		// In eclipse use Ctrl+I to indent multiple selected lines.
 
-    protected void interrupted() {
-    	end();
-    }
+		if (table.getNumber("heartbeat", 0) == 1) {
+
+			double output = Robot.turntable.getAngle() + (factor * pixelError);
+			Robot.turntable.setAngle(output);
+			SmartDashboard.putNumber("pid error", Robot.turntable.getSetpoint() - RobotMap.turntablePIDSource.pidGet());
+			
+			
+			/*if (!continuous && Robot.turntable.onTarget()) {
+				if (!continuous && Math.abs(pixelError) < 20) {
+					if (!onTargetForReal) {
+						setTimeout(1);
+					}
+
+					onTargetForReal = true;
+				} else {
+					onTargetForReal = false;
+				}
+			}*/
+			
+			Robot.turntable.setAngle(output);
+			SmartDashboard.putNumber("output for turntable", output);
+		}	
+	}
+
+	protected boolean isFinished() {
+		if (table.getNumber("heartbeat",0)==1) {
+			table.putNumber("heartbeat",0);
+			if (Robot.turntable.onTarget() && isTimedOut() && onTargetForReal) {
+				SmartDashboard.putBoolean("Shoot Horizontally Aligned", true);
+				if (continuous == false) { return true; }
+			} else {
+				SmartDashboard.putBoolean("Shoot Horizontally Aligned", false);
+			}
+		} else {
+			
+		}
+		return false;
+	}
+
+	protected void end() {
+		SmartDashboard.putBoolean("Shoot Horizontally Aligned", false);
+	}
+
+	protected void interrupted() {
+		end();
+	}
 }
