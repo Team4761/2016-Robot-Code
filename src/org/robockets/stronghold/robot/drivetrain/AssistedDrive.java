68631f898b3b277b5167f1d4fe82355f466ec595
Brian Shin <brianshin100@yahoo.com>
2016-03-21 21:41:02 -0400

    Add javadocs for AssistedDrive.java

--- src/org/robockets/stronghold/robot/drivetrain/AssistedDrive.java
+++ src/org/robockets/stronghold/robot/drivetrain/AssistedDrive.java
@@ -5,7 +5,7 @@ import org.robockets.stronghold.robot.Robot;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
- *
+ * Command used for self-driving with PID control.
  */
 public class AssistedDrive extends Command {
 	final double CONSTANT_DISTANCE_UPDATE = 10 * 0.02;
@@ -17,6 +17,14 @@ public class AssistedDrive extends Command {
 	double relativeAngle;
 	boolean cutOnHighSpeed;
 	
+	/**
+	 * Main constructor to control robot driving with PID.
+	 * @param translatePidType Choose from a few Enums on how you want to move. There's ENCODER which uses encoders to track distance and NONE.
+	 * @param rotationPidType More Enums to choose from which controls how one will turn, including: GYRO-using gyroscope, COMPASS-nonexistant but uses compass, and ENCODER which uses encoders to keep straight.
+	 * @param distance This is in inches and how far you would like to go.
+	 * @param relativeAngle Angle relative to robot that you want to turn.
+	 * @param inchesPerSecond Rate of speed in inches per second. Full speed is 162 inches per second.
+	 */
     public AssistedDrive(AssistedTranslateType translatePidType, AssistedRotateType rotationPidType, double distance, double relativeAngle, double inchesPerSecond) {
         requires(Robot.driveTrain);
         
@@ -29,12 +37,26 @@ public class AssistedDrive extends Command {
         this.inchesPerSecond = inchesPerSecond;
     }
     
+    /**
+     * Constructor for PID compensation, where it will stop at desired setpoint without slowing down before.
+     * @param translatePidType Choose from a few Enums on how you want to move. There's ENCODER which uses encoders to track distance and NONE.
+	 * @param rotationPidType More Enums to choose from which controls how one will turn, including: GYRO-using gyroscope, COMPASS-nonexistant but uses compass, and ENCODER which uses encoders to keep straight.
+	 * @param cutOnHighSpeed True to make the setpoint larger than set for speed, then stops at real desired setpoint without slowing down. Don't use this constructor if false.
+	 * @param distance This is in inches and how far you would like to go.
+	 * @param relativeAngle Angle relative to robot that you want to turn.
+	 * @param inchesPerSecond Rate of speed in inches per second. Full speed is 162 inches per second.
+     */
     public AssistedDrive(AssistedTranslateType translatePidType, AssistedRotateType rotationPidType, boolean cutOnHighSpeed, double distance, double relativeAngle, double inchesPerSecond) {
         this(translatePidType, rotationPidType, distance, relativeAngle, inchesPerSecond);
         
         this.cutOnHighSpeed = cutOnHighSpeed;
     }
     
+    /**
+     * Constructor for rotating in place.
+     * @param rotatePidType More Enums to choose from which controls how one will turn, including: GYRO-using gyroscope, COMPASS-nonexistant but uses compass, and ENCODER which uses encoders to keep straight.
+     * @param relativeAngle Angle relative to robot that you want to turn.
+     */
     public AssistedDrive(AssistedRotateType rotatePidType, double relativeAngle) {
     	this(AssistedTranslateType.NONE, rotatePidType, 0.0, relativeAngle, 0.0);
     }
