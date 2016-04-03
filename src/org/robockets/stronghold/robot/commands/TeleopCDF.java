a16d28a070282004666275f976ddf38ed8b5438b
Brian Shin <brianshin100@yahoo.com>
2016-04-03 10:44:18 -0400

    Add command for CDF in teleop

---
+++ src/org/robockets/stronghold/robot/commands/TeleopCDF.java
@@ -0,0 +1,20 @@
+package org.robockets.stronghold.robot.commands;
+
+import org.robockets.stronghold.robot.drivetrain.AssistedDrive;
+import org.robockets.stronghold.robot.drivetrain.AssistedRotateType;
+import org.robockets.stronghold.robot.drivetrain.AssistedTranslateType;
+import org.robockets.stronghold.robot.intake.ChevalDeFrise;
+import org.robockets.stronghold.robot.intake.IntakeSide;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ * Command group for CDF in teleop. Driver needs to crash into CDF, then operator presses the button from there.
+ */
+public class TeleopCDF extends CommandGroup {
+    
+    public  TeleopCDF(IntakeSide intakeSide) {
+        addSequential(new AssistedDrive(AssistedTranslateType.ENCODER, AssistedRotateType.ENCODER, 5, 0, 12));
+        addSequential(new ChevalDeFrise(intakeSide));
+    }
+}
